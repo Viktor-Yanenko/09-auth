5c048802896413e8f454ce90fa6e5d06a3fd0532
Viktor <viktoryanenko11@gmail.com>
2025-07-11 13:50:02 +0300

    add signin page, add any styles

--- app/(auth routes)/sign-in/page.tsx
+++ app/(auth routes)/sign-in/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import css from './SignUpPage.module.css';
+import css from './SignInPage.module.css';
 
-export default function SignUpPage() {
+export default function SignInPage() {
   return (
     <main className={css.mainContent}>
-      <h1 className={css.formTitle}>Sign up</h1>
       <form className={css.form}>
+        <h1 className={css.formTitle}>Sign in</h1>
+
         <div className={css.formGroup}>
           <label htmlFor="email">Email</label>
           <input
@@ -17,6 +18,7 @@ export default function SignUpPage() {
             required
           />
         </div>
+
         <div className={css.formGroup}>
           <label htmlFor="password">Password</label>
           <input
@@ -27,12 +29,14 @@ export default function SignUpPage() {
             required
           />
         </div>
+
         <div className={css.actions}>
           <button type="submit" className={css.submitButton}>
-            Register
+            Log in
           </button>
         </div>
-        <p className={css.error}>Error</p>
+
+        {/* <p className={css.error}>{error}</p> */}
       </form>
     </main>
   );
